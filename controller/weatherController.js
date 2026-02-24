6f8a7257a581f9de19395e1a4c4c1ce9745f75c5
sarita595k <sarita595k@gmail.com>
2026-02-24 19:39:24 +0530

    user live location detected and then the home page updated

--- controller/weatherController.js
+++ controller/weatherController.js
@@ -30,3 +30,20 @@ export const getWeather = async (req, res) => {
         });
     }
 };
+
+export const getCurrentWeather = async (req, res) => {
+    try {
+        const { lat, lon } = req.query;
+
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=metric`
+        );
+        const data = await response.json();
+        res.status(200).json(data);
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching weather"
+        });
+    }
+}
--- route/weatherRoute.js
+++ route/weatherRoute.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { getWeather } from "../controller/weatherController.js";
+import { getCurrentWeather, getWeather } from "../controller/weatherController.js";
 const route = Router()
+
+// getting user current location 
+route.get("/current", getCurrentWeather)
 
 // route api/weather/:city
 route.get("/:city", getWeather)
-
 export default route
