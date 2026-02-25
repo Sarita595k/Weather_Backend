8ff3bd21e0790cb8c8daee4b83c22a7c8b091282
sarita595k <sarita595k@gmail.com>
2026-02-25 13:19:33 +0530

    nodemailer code changed

--- middleware/nodemailer.js
+++ middleware/nodemailer.js
@@ -1,20 +1,19 @@
-// Nodemailer setup
+// nodemailer.js
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    }
-})
+        pass: process.env.EMAIL_PASS // App password
+    },
+    family: 4, // force IPv4
+});
 
-export const generateMailOptions = (userEmail, resetUrl) => {
-    return {
-        from: process.env.EMAIL_USER,
-        to: userEmail,
-        subject: "Password Reset Request",
-        html: `<p>Click <a href="${resetUrl}">here</a> to reset your password.</p>
-           <p>Link expires in 1 hour.</p>`,
-    };
-};
+export const generateMailOptions = (userEmail, resetUrl) => ({
+    from: process.env.EMAIL_USER,
+    to: userEmail,
+    subject: "Password Reset Request",
+    html: `<p>Click <a href="${resetUrl}">here</a> to reset your password.</p>
+         <p>Link expires in 1 hour.</p>`
+});
