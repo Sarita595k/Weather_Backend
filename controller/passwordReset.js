09e3a326e34f5d060e43378c56e44c9e427b41a7
sarita595k <sarita595k@gmail.com>
2026-02-25 13:48:09 +0530

    remove the local link from forgot password

--- controller/passwordReset.js
+++ controller/passwordReset.js
@@ -1,50 +1,48 @@
-// userController.js
 import { generateMailOptions, transporter } from "../middleware/nodemailer.js";
 import { UserDetails } from "../model/userModel.js";
 
+
 export const forgotPassword = async (req, res) => {
     const { email } = req.body;
-    try {
-        const user = await UserDetails.findOne({ email });
-        if (!user) return res.status(404).json({ message: "User not found" });
 
-        const token = Math.random().toString(36).substring(2, 15);
-        user.resetPasswordToken = token;
-        user.resetPasswordExpire = Date.now() + 3600000; // 1 hour
-        await user.save();
+    const user = await UserDetails.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-        const resetUrl = `https://avsarweather.netlify.app/reset-password/${token}`;
-        const mailOptions = generateMailOptions(user.email, resetUrl);
+    const token = Math.random().toString(36).substring(2, 15);
+    user.resetPasswordToken = token;
+    user.resetPasswordExpire = Date.now() + 36000;
+    await user.save();
 
-        await transporter.sendMail(mailOptions);
+    const resetUrl = `https://avsarweather.netlify.app/reset-password/${token}`;
 
+    const mailOptions = generateMailOptions(user.email, resetUrl);
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ message: "Email could not be sent" });
+        }
         res.status(200).json({ message: "Password reset email sent" });
-    } catch (err) {
-        console.log("Forgot password error:", err);
-        res.status(500).json({ message: "Email could not be sent" });
-    }
-};
-
+    });
+}
+// PUT /api/user/reset-password/:token
 export const resetPasswordLink = async (req, res) => {
     const { token } = req.params;
     const { password } = req.body;
 
-    try {
-        const user = await UserDetails.findOne({
-            resetPasswordToken: token,
-            resetPasswordExpire: { $gt: Date.now() },
-        });
+    const user = await UserDetails.findOne({
+        resetPasswordToken: token,
+        resetPasswordExpire: { $gt: Date.now() }, // check token expiry
+    });
 
-        if (!user) return res.status(400).json({ message: "Invalid or expired token" });
+    if (!user) return res.status(400).json({ message: "Invalid or expired token" });
 
-        user.password = password; // store plain password for learning
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpire = undefined;
+    // Update password
+    user.password = password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
 
-        await user.save();
-        res.status(200).json({ message: "Password reset successful" });
-    } catch (err) {
-        console.log("Reset password error:", err);
-        res.status(500).json({ message: "Server error" });
-    }
-};
+    await user.save();
+
+    res.status(200).json({ message: "Password reset successful" });
+}
